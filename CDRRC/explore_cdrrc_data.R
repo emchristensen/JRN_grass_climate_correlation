a9568a5eb517525638ec1f9b55d1cec85158ea3d
emchristensen <erica.christensen@weecology.org>
2021-07-08 16:41:56 -0600

    gap filled grass timeseries with CDRRC regression

--- CDRRC/explore_cdrrc_data.R
+++ CDRRC/explore_cdrrc_data.R
@@ -18,3 +18,22 @@ forage
 ggsave('Figures/CDRRC_forage.png', plot=forage, width=5, height=3)
 
 # best forage years were during the 1978-1999 warm PDO phase. recent PDO phase 2015-2018 very low production
+
+# read in quadrat time series
+yearlygrass = read.csv('data/grass_shrub_trends_yearly.csv')
+
+# plot quadrat cover vs forage
+quadforage = merge(dat, yearlygrass, by.x='year', by.y='project_year')
+quadforageplot = ggplot(quadforage, aes(x=forage.production..kg.ha.1., y=mean_grass)) +
+  geom_point() +
+  geom_text(aes(label=year)) +
+  geom_smooth(method='lm', formula=y~x) +
+  xlab('CDRRC forage production (kg/ha)') +
+  ylab('quadrat grass cover')
+quadforageplot
+ggsave('Figures/CDRRC_forage_vs_quad_cover.png', plot=quadforageplot, width=5, height=5)
+
+
+test = lm(quadforage$mean_grass ~ quadforage$forage.production..kg.ha.1.)
+summary(test)
+# R-squared on the linear correlation is around 0.3
---
+++ CDRRC/model_cover_on_CDRRC.R
@@ -0,0 +1,36 @@
+#' Model grass cover based on forage production 1980-1994
+#' EMC 7/8/21
+
+library(dplyr)
+library(ggplot2)
+
+# read in CDRRC forage data
+forage = read.csv('CDRRC/CDRRC pasture 1 long term forage production.csv', skip=2)
+# read in quadrat time series
+yearlygrass = read.csv('data/grass_shrub_trends_yearly.csv')
+
+# combine data; create linear model based on overlap
+foragecover = merge(forage, yearlygrass, by.x='year', by.y='project_year') %>%
+  dplyr::select(year, mean_grass, forage=forage.production..kg.ha.1.)
+
+model1 = lm(mean_grass ~ forage, data=foragecover)
+summary(model1)
+
+# predict cover values 1980-1994
+forage_1980_1994 = dplyr::filter(forage, year %in% 1980:1994) %>%
+  dplyr::select(year, forage=forage.production..kg.ha.1.) %>%
+  dplyr::mutate(mean_grass = NA)
+
+p = predict(test, forage_1980_1994)
+forage_1980_1994$mean_grass = p
+
+# combine into grass timeseries
+yearlygrass_gapfilled = yearlygrass %>%
+  dplyr::select(year=project_year, mean_grass) %>%
+  merge(forage_1980_1994, all=T)
+
+# plot new time series
+ggplot(yearlygrass_gapfilled, aes(x=as.numeric(year), y=mean_grass)) +
+  geom_point() +
+  geom_line()
+write.csv(yearlygrass_gapfilled, 'CDRRC/yearly_grass_gapfilled.csv', row.names=F)
