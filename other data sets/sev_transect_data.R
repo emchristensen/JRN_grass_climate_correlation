89708dc8be07ea98818af199ca74cb7697e16856
emchristensen <erica.christensen@weecology.org>
2021-08-12 12:14:54 -0600

    finished looking at sev data. fire effect?

--- other data sets/sev_transect_data.R
+++ other data sets/sev_transect_data.R
@@ -25,17 +25,55 @@ rawdat_sp = merge(rawdat, splist, by.x='species', by.y='kartez', all.x=T)
 rawdat_grass = rawdat_sp %>%
   dplyr::filter(FunctionalGroup %in% c('g',NA), LifeHistory %in% c('p','ap',''), condition==2)
 
-# look at a single transect as a test case
-test = rawdat_grass %>%
-  dplyr::filter(year==1989, location=='DW',period=='1')
 
 #' try: multiply start/stop by 100 to get integers. each plant spans a sequence of integers. get unique 
 #' set of integers per transect that are covered by any plant (use unique to get rid of duplicates=overlaps)
-test$startcm = test$start*100
-test$stopcm = test$stop*100
-grass_cm = c()
-for (n in 1:nrow(test)) {
-  grassseq = test$startcm[n]:test$stopcm[n]
-  grass_cm = c(grass_cm, grassseq)
+
+rawdat_grass$startcm = rawdat_grass$start*100
+rawdat_grass$stopcm = rawdat_grass$stop*100
+
+# unique transect determined by location, year, season
+
+# data frame to hold total grass cover per transect
+transects_totalgrass = c()
+# loop through each location, year, and season (period)
+for (loc in unique(rawdat_grass$location)) {
+  locationdat = dplyr::filter(rawdat_grass, location==loc)
+  for (y in unique(locationdat$year)) {
+    yeardat = dplyr::filter(locationdat, year==y)
+    for (s in unique(yeardat$period)) {
+      perioddat = dplyr::filter(yeardat, period==s)
+      
+      # vector to hold sequence of locations covered by grass on this transect
+      grass_locations = c() 
+      for (n in 1:nrow(perioddat)) {
+        grassseq = perioddat$startcm[n]:perioddat$stopcm[n]
+        grass_locations = c(grass_locations, grassseq)
+      }
+      # get unique grass_locations, save to data frame
+      totalgrass_cm = length(unique(grass_locations))/100
+      transects_totalgrass = rbind(transects_totalgrass, data.frame(location=loc, year=y, period=s, total_grass=totalgrass_cm))
+    }
+  }
 }
-totalgrass_cm = length(unique(grass_cm))/100
+
+write.csv(transects_totalgrass,'other data sets/SEV_transects_1989_2019.csv', row.names=F)
+
+# =================================================================================
+# plot time series
+
+transects_totalgrass = read.csv('other data sets/SEV_transects_1989_2019.csv')
+
+dw_spring = dplyr::filter(transects_totalgrass, period==1, location=='DW')
+ggplot(dw_spring, aes(x=year, y=total_grass)) +
+  geom_line() +
+  geom_point() +
+  theme_bw()
+# clear big dip in 2009 for fire
+
+fp_spring = dplyr::filter(transects_totalgrass, period==1, location=='FP')
+ggplot(fp_spring, aes(x=year, y=total_grass)) +
+  geom_line() +
+  geom_point() +
+  theme_bw()
+# big dip in 2011. Why?
